43b18638fb267d7a7bbace8edf610036b37cee14
FedericoCalzoni <federicocalzoni01@gmail.com>
2024-03-24 15:26:13 +0100

    fix

--- folders.sh
+++ folders.sh
@@ -10,16 +10,17 @@
 function sync_folder() {
     local source_folder="$1"
     local remote_folder="$2"
-	local hostname=$(hostname)
-	
-	shift 2  # Shift to remove the first two arguments
-	echo "Syncing $source_folder to $remote_folder"
-	echo "";
+    
+    shift 2  # Shift to remove the first two arguments
+    echo "";
+    echo "Syncing $source_folder to $remote_folder"
     /usr/bin/rclone sync "$source_folder" "$hostname:$remote_folder" "$@" --fast-list --max-backlog 200000 --progress
 }
 
+hostname=$(uname -n)
+
 if [ "$#" -lt 1 ]; then
-	echo ""
+    echo ""
     echo "Usage: $0 <do_sync_confetc>"
     exit 1
 fi
@@ -27,126 +28,135 @@ fi
 # Access the input parameters
 do_sync_confetc="$1"
 
-echo ""
-echo "Sync of Desktop folder"
-sync_folder "/home/$USER/Desktop/" "$hostname/Desktop"
-
-
-echo ""
-echo "Sync of Documents folder"
+# Sync Desktop folder
 parameters=(
-	# "--skip-links"
+    # "--skip-links"
     # "--exclude=/foo1/foo2/**"
     # "--exclude=/foo3/**"
-	# "--exclude=/.**"
+    # "--exclude=/.**"
     # "--exclude=*cache/**"
     # "--exclude=*Cache/**"
     # "--exclude=*tmp/**"
     # "--exclude=*metadata/**"
     # "--exclude=*.tmp"
-	# "--exclude=*log/**"
-	# "--exclude=*LOG/**"
+    # "--exclude=*log/**"
+    # "--exclude=*LOG/**"
+    # "--exclude=*.log"
+    "--delete-excluded"
+)
+sync_folder "/home/$USER/Desktop/" "$hostname/Desktop" "${parameters[@]}"
+
+
+# Sync Documents folder
+parameters=(
+    # "--skip-links"
+    # "--exclude=/foo1/foo2/**"
+    # "--exclude=/foo3/**"
+    # "--exclude=/.**"
+    # "--exclude=*cache/**"
+    # "--exclude=*Cache/**"
+    # "--exclude=*tmp/**"
+    # "--exclude=*metadata/**"
+    # "--exclude=*.tmp"
+    # "--exclude=*log/**"
+    # "--exclude=*LOG/**"
     # "--exclude=*.log"
     "--delete-excluded"
 )
 sync_folder "/home/$USER/Documents/" "$hostname/Documents" "${parameters[@]}"
 
-echo ""
-echo "Sync of Music folder"
+# Sync Music folder
 parameters=(
-	# "--skip-links"
-	# "--exclude=/foo4/foo5/**"
-	# "--exclude=/.**"
-	# "--exclude=*cache/**"
+    # "--skip-links"
+    # "--exclude=/foo4/foo5/**"
+    # "--exclude=/.**"
+    # "--exclude=*cache/**"
     # "--exclude=*Cache/**"
     # "--exclude=*tmp/**"
     # "--exclude=*metadata/**"
     # "--exclude=*.tmp"
     # "--exclude=*log/**"
-	# "--exclude=*LOG/**"
+    # "--exclude=*LOG/**"
     # "--exclude=*.log"
     "--delete-excluded"
 )
 sync_folder "/home/$USER/Music/" "$hostname/Music" "${parameters[@]}"
 
-echo ""
-echo "Sync of Pictures folder"
+# Sync Pictures folder
 parameters=(
-	# "--skip-links"
-	# "--exclude=/foo4/foo5/**"
-	# "--exclude=/.**"
-	# "--exclude=*cache/**"
+    # "--skip-links"
+    # "--exclude=/foo4/foo5/**"
+    # "--exclude=/.**"
+    # "--exclude=*cache/**"
     # "--exclude=*Cache/**"
     # "--exclude=*tmp/**"
     # "--exclude=*metadata/**"
     # "--exclude=*.tmp"
     # "--exclude=*log/**"
-	# "--exclude=*LOG/**"
+    # "--exclude=*LOG/**"
     # "--exclude=*.log"
     "--delete-excluded"
 )
 sync_folder "/home/$USER/Pictures/" "$hostname/Pictures" "${parameters[@]}"
 
-echo ""
-echo "Sync of Videos folder"
+# Sync Videos folder
 parameters=(
-	# "--skip-links"
-	# "--exclude=/foo5/**"
-	# "--exclude=/.**"
-	# "--exclude=*cache/**"
+    # "--skip-links"
+    # "--exclude=/foo5/**"
+    # "--exclude=/.**"
+    # "--exclude=*cache/**"
     # "--exclude=*Cache/**"
     # "--exclude=*tmp/**"
     # "--exclude=*metadata/**"
     # "--exclude=*.tmp"
     # "--exclude=*log/**"
-	# "--exclude=*LOG/**"
+    # "--exclude=*LOG/**"
     # "--exclude=*.log"
     "--delete-excluded"
 )
 sync_folder "/home/$USER/Videos/" "$hostname/Videos" "${parameters[@]}"
-	
 
+# Sync .config and /etc folders if the user chooses to do so
 if [[ $do_sync_confetc =~ ^(y|Y|)$ ]]; then
-	echo ""
-	echo "Sync of Config folder"
-	parameters=(
-		# "--skip-links"
-		# "--exclude=/foo/**"
-	    # "--exclude=*cache/**"
-   		# "--exclude=*Cache/**"
-   		# "--exclude=CachedData/**"
-    	# "--exclude=*tmp/**"
-    	# "--exclude=*metadata/**"
-    	# "--exclude=*.tmp"
-    	# "--exclude=*log/**"
-		# "--exclude=*LOG/**"
-		# "--exclude=*log.*"
-		# "--exclude=logs/**"
-    	# "--exclude=*.log"
-    	"--delete-excluded"
-	)
-	sync_folder "/home/$USER/.config/" "$hostname/.config" "${parameters[@]}"
-	    
-	echo ""
-	echo "Sync of etc folder"
-	parameters=(
-		# "--skip-links"
-	    # "--exclude=sane.d/**"
-	    # "--exclude=*cache/**"
-    	# "--exclude=*Cache/**"
-    	# "--exclude=*tmp/**"
-    	# "--exclude=*metadata/**"
-    	# "--exclude=*.tmp"
-    	# "--exclude=*log/**"
-		# "--exclude=*LOG/**"
-    	# "--exclude=*.log"
-    	# "--exclude=*.key"
-    	# "--exclude=key.*"
-    	"--delete-excluded"
-		"--ignore-errors"
-	)
-	sync_folder "/etc/" "$hostname/etc" "${parameters[@]}"
-	    
+    
+    # Sync /.config folder
+    parameters=(
+        # "--skip-links"
+        # "--exclude=/foo/**"
+        # "--exclude=*cache/**"
+        # "--exclude=*Cache/**"
+        # "--exclude=CachedData/**"
+        # "--exclude=*tmp/**"
+        # "--exclude=*metadata/**"
+        # "--exclude=*.tmp"
+        # "--exclude=*log/**"
+        # "--exclude=*LOG/**"
+        # "--exclude=*log.*"
+        # "--exclude=logs/**"
+        # "--exclude=*.log"
+        "--delete-excluded"
+    )
+    sync_folder "/home/$USER/.config/" "$hostname/.config" "${parameters[@]}"
+    
+    # Sync /etc folder
+    parameters=(
+        # "--skip-links"
+        # "--exclude=sane.d/**"
+        # "--exclude=*cache/**"
+        # "--exclude=*Cache/**"
+        # "--exclude=*tmp/**"
+        # "--exclude=*metadata/**"
+        # "--exclude=*.tmp"
+        # "--exclude=*log/**"
+        # "--exclude=*LOG/**"
+        # "--exclude=*.log"
+        # "--exclude=*.key"
+        # "--exclude=key.*"
+        "--delete-excluded"
+        "--ignore-errors"
+    )
+    sync_folder "/etc/" "$hostname/etc" "${parameters[@]}"
+    
 fi
 echo ""
 echo "COMPLETED SYNC"
